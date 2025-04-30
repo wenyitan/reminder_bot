5b8205324ff01d03090c91b3df128b7f7fe5f905
wenyitan <tanwenyi1511@gmail.com>
2025-05-01 01:14:13 +0800

    refactor database to address recursive usage of cursor

--- bot/database.py
+++ bot/database.py
@@ -7,31 +7,40 @@ def dict_factory(cursor, row):
 
 class Database:
     def __init__(self, db_path="/app/bot/reminders.db"):
-        self.conn = sqlite3.connect(db_path, check_same_thread=False)
-        self.conn.row_factory = dict_factory
-        self.cursor = self.conn.cursor()
+        self.db_path=db_path
+
+    def _get_connection(self):
+        conn = sqlite3.connect(self.db_path, check_same_thread=False)
+        conn.row_factory = dict_factory
+        return conn
 
     def execute(self, query, values=None):
-        self.cursor.execute(query, values or ())
-        self.conn.commit()
+        with self._get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(query, values or ())
+            conn.commit()
 
     def fetch_all(self, query, values=None):
-        self.cursor.execute(query, values or ())
-        return self.cursor.fetchall()
+        with self._get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(query, values or ())
+            return cursor.fetchall()
     
     def fetch_one(self, query, values=None):
-        self.cursor.execute(query, values or ())
-        return self.cursor.fetchone()
-
-    def close(self):
-        self.conn.close()
+        with self._get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(query, values or ())
+            return cursor.fetchone()
 
     def init_db(self):
-        create_table_query = f"""
-            CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
-            user_id TEXT PRIMARY KEY,
-            chat_id TEXT NOT NULL,
-            hour INTEGER NOT NULL
-            );
-        """
-        self.cursor.execute(create_table_query)
+        with self._get_connection() as conn:
+            cursor = conn.cursor()
+            create_table_query = f"""
+                CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
+                user_id TEXT PRIMARY KEY,
+                chat_id TEXT NOT NULL,
+                hour INTEGER NOT NULL
+                );
+            """
+            cursor.execute(create_table_query)
+            conn.commit()
